8ad3e0948e96525d6f4a16c29ea20057e1b21692
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2020-10-29 14:25:14 -0700

    setting up

---
+++ public/scripts/index.js
@@ -0,0 +1,12 @@
+console.log("sanity check")
+
+// module.exports = {
+//     views: require("./viewRoutes")
+// };
+
+// moving carousel 
+
+// $('.carousel').carousel({
+// 	interval: 1000
+// })
+
